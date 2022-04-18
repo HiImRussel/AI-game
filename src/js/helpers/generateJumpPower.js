dbbb215b7f6e419fced80d5a9d3478674da31e82
Russel <russel1234561@gmail.com>
2022-04-18 13:48:11 +0200

    poprawki AI generowanie mocy skoku

--- src/js/helpers/generateJumpPower.js
+++ src/js/helpers/generateJumpPower.js
@@ -1,6 +1,34 @@
 import { AI_DATA, getClosest } from "../store.js";
 import getRandomBetween from "./getRandomBetween.js";
 
+const generateForDirection = (
+    jumpData,
+    dataDiff,
+    lowestPower,
+    highestPower
+) => {
+    if (lowestPower > 1) {
+        lowestPower = parseFloat(jumpData.fail.jumpPower).toFixed(2);
+    }
+    if (highestPower > 1) {
+        highestPower = parseFloat(jumpData.success.jumpPower).toFixed(2);
+    }
+
+    let jumpPower = parseFloat(
+        getRandomBetween(lowestPower, highestPower)
+    ).toFixed(2);
+
+    if (lowestPower > highestPower) {
+        jumpPower = parseFloat(getRandomBetween(0, highestPower)).toFixed(2);
+    }
+
+    if (dataDiff === 1) {
+        jumpPower = parseFloat(jumpData.success.jumpPower).toFixed(2);
+    }
+
+    return jumpPower;
+};
+
 const generateJumpPower = (distanceData) => {
     if (AI_DATA.length === 0) return parseFloat(Math.random() * 1).toFixed(2);
 
@@ -12,16 +40,33 @@ const generateJumpPower = (distanceData) => {
         parseFloat(jumpData.success.distanceToJump.between).toFixed(2) /
         parseFloat(distanceData.between).toFixed(2);
 
-    let jumpAfterDif =
-        dataDiff * parseFloat(jumpData.success.jumpPower).toFixed(2);
-    if (jumpAfterDif > 1) {
-        jumpAfterDif = 1;
+    let lowestPower = parseFloat(jumpData.fail.jumpPower).toFixed(2) * dataDiff;
+    let highestPower =
+        parseFloat(jumpData.success.jumpPower).toFixed(2) * dataDiff;
+
+    let jumpPower = 0;
+
+    if (distanceData.direction === "up") {
+        jumpPower = generateForDirection(
+            jumpData,
+            dataDiff,
+            lowestPower,
+            highestPower
+        );
+    } else {
+        jumpPower = generateForDirection(
+            jumpData,
+            dataDiff,
+            highestPower,
+            lowestPower
+        );
     }
 
-    return getRandomBetween(
-        jumpAfterDif,
-        parseFloat(jumpData.success.jumpPower).toFixed(2)
-    );
+    console.log(distanceData);
+    console.log(jumpData);
+    console.log(jumpPower);
+
+    return jumpPower;
 };
 
 export default generateJumpPower;
